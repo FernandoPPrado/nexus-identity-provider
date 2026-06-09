4b490f497cc93ebc0778e1a8fcfce699b83a88c2
Fernando Prado <fernandopradoifc@gmail.com>
2026-06-09 20:24:45 -0300

    feature(security): implementa service responsável por gerar os tokens assinados

--- src/main/java/com/fernando/iop/security/service/TokenService.java
+++ src/main/java/com/fernando/iop/security/service/TokenService.java
@@ -1,15 +1,29 @@
 package com.fernando.iop.security.service;
 
 import com.fernando.iop.user.dto.UserEntityResponseDTO;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class TokenService {
 
-    public String generateToken(UserEntityResponseDTO userEntityResponseDTO){
+    private final JwtEncoder jwtEncoder;
 
+    public TokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
 
+    public String generateToken(UserEntityResponseDTO userEntityResponseDTO) {
+        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder().issuer("idp").subject(userEntityResponseDTO.userId().toString()).issuedAt(Instant.now()).
+                expiresAt(Instant.now().plusSeconds(3600)).
+                claim("projectId", userEntityResponseDTO.projectId()).
+                claim("email", userEntityResponseDTO.userEmail()).claim("roles", userEntityResponseDTO.userRoles()).build();
 
+        return jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
     }
 
 }
