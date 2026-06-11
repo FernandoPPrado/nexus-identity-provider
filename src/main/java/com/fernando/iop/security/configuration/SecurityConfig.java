0d43089a3ff6fc672b6a0fafb6c8f7412655738e
Fernando Prado <fernandopradoifc@gmail.com>
2026-06-11 20:51:14 -0300

    feat(security): implementa SecurityFilterChain e demais configuraçoes de segurança

---
+++ src/main/java/com/fernando/iop/security/configuration/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.fernando.iop.security.configuration;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                // 1. Desabilita CSRF por ser uma API Stateless
+                .csrf(csrf -> csrf.disable())
+
+                // 2. Define a política de sessão como STATELESS
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // 3. FAXINA: Desabilita o form HTML tradicional e o Http Basic do navegador
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+
+                // 4. Regras de autorização das rotas
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                // 5. Configura o Resource Server para aceitar e validar os JWTs
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                }))
+
+                .build();
+
+
+    }
+
+
+}
