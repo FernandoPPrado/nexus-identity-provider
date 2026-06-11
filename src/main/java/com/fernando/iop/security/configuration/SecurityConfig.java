a521758a9abe632210e81c8cec67a93fe930549c
Fernando Prado <fernandopradoifc@gmail.com>
2026-06-11 20:52:58 -0300

    feat(security): implementa @Bean PasswordEncoder

--- src/main/java/com/fernando/iop/security/configuration/SecurityConfig.java
+++ src/main/java/com/fernando/iop/security/configuration/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.fernando.iop.security.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -36,5 +43,9 @@ public class SecurityConfig {
 
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
