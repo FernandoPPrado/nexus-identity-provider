ae439a5f307bd2907041b6ffd464791bd730f0a9
Fernando Prado <fernandopradoifc@gmail.com>
2026-06-13 21:05:51 -0300

    refactor(Project): altera projectId para campo project

--- src/main/java/com/fernando/iop/user/service/UserService.java
+++ src/main/java/com/fernando/iop/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.fernando.iop.user.service;
 
+import com.fernando.iop.project.model.Project;
+import com.fernando.iop.project.repository.ProjectRepository;
 import com.fernando.iop.user.dto.UserEntityResponseDTO;
 import com.fernando.iop.user.enums.UserRoles;
 import com.fernando.iop.user.model.User;
@@ -14,24 +16,26 @@ import java.util.UUID;
 public class UserService {
 
     private final UserH2Repository userH2Repository;
+    private final ProjectRepository projectRepository;
 
-    public UserService(UserH2Repository userH2Repository) {
+    public UserService(UserH2Repository userH2Repository, ProjectRepository projectRepository) {
         this.userH2Repository = userH2Repository;
+        this.projectRepository = projectRepository;
     }
 
 
-    public UserEntityResponseDTO findUserByEmailAndProjectId(String email, UUID projectId) {
-        User user = userH2Repository.findByUserEmailAndProjectId(email, projectId).orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
-        return new UserEntityResponseDTO(user.getUserEmail(), user.getUserId(), user.getProjectId(), user.getUserRoles());
+    public UserEntityResponseDTO findUserByEmailAndProjectId(String email, Project project) {
+        User user = userH2Repository.findByUserEmailAndProjectId(email, project.getProjectId()).orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
+        return new UserEntityResponseDTO(user.getUserEmail(), user.getUserId(), user.getProject(), user.getUserRoles());
     }
 
 
-    public void createUser(String email, String password, UUID projectId, UserRoles roles) {
+    public void createUser(String email, String password, Project project, UserRoles roles) {
 
-        if (userH2Repository.existsByUserEmailAndProjectId(email, projectId)) {
+        if (userH2Repository.existsByUserEmailAndProjectId(email, project.getProjectId()) || !projectRepository.existsByProjectId(project.getProjectId())) {
             throw new EntityExistsException();
         }
-        userH2Repository.save(new User(email, password, UserRoles.ROLE_USER, projectId));
+        userH2Repository.save(new User(email, password, UserRoles.ROLE_USER, project));
 
 
     }
