bab82f9230945411ac36e6df06e8ab7fe4ef09b5
Fernando Prado <fernandopradoifc@gmail.com>
2026-06-14 21:44:55 -0300

    refector(user): aplica mudanças nos nomes dos metodos de buscas para acesso ao id do Projeto

--- src/main/java/com/fernando/iop/user/service/UserService.java
+++ src/main/java/com/fernando/iop/user/service/UserService.java
@@ -25,14 +25,14 @@ public class UserService {
 
 
     public UserEntityResponseDTO findUserByEmailAndProjectId(String email, Project project) {
-        User user = userH2Repository.findByUserEmailAndProjectId(email, project.getProjectId()).orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
+        User user = userH2Repository.findByUserEmailAndProject_ProjectId(email, project.getProjectId()).orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
         return new UserEntityResponseDTO(user.getUserEmail(), user.getUserId(), user.getProject(), user.getUserRoles());
     }
 
 
     public void createUser(String email, String password, Project project, UserRoles roles) {
 
-        if (userH2Repository.existsByUserEmailAndProjectId(email, project.getProjectId()) || !projectRepository.existsByProjectId(project.getProjectId())) {
+        if (userH2Repository.existsByUserEmailAndProject_ProjectId(email, project.getProjectId()) || !projectRepository.existsByProjectId(project.getProjectId())) {
             throw new EntityExistsException();
         }
         userH2Repository.save(new User(email, password, UserRoles.ROLE_USER, project));
