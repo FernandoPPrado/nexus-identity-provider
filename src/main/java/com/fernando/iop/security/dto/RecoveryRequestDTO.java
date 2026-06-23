1a9d3c334fba3eb6d9f479b6fb22bc12b71de63a
Fernando Prado <fernandopradoifc@gmail.com>
2026-06-23 19:51:05 -0300

    feat(Security): implementa validaçoes para os campos dos DTOs

--- src/main/java/com/fernando/iop/security/dto/RecoveryRequestDTO.java
+++ src/main/java/com/fernando/iop/security/dto/RecoveryRequestDTO.java
@@ -1,12 +1,25 @@
 package com.fernando.iop.security.dto;
 
+import jakarta.validation.constraints.*;
+
 import java.util.UUID;
 
 public record RecoveryRequestDTO(
 
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "O formato do e-mail é inválido")
+        @Size(min = 11, max = 64, message = "O e-mail deve ter entre 11 e 64 caracteres")
         String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 6, max = 64, message = "A senha deve ter entre 6 e 64 caracteres")
         String password,
+
+        @NotNull(message = "O ID do projeto é obrigatório")
         UUID projectId,
+
+        @NotBlank(message = "O token de recuperação é obrigatório")
+        @Pattern(regexp = "^\\d{6}$", message = "O token de recuperação deve conter exatamente 6 dígitos numéricos")
         String recoveryToken
 
 ) {
--- src/main/java/com/fernando/iop/user/dto/UserRecoveryConfirmRequestDTO.java
+++ src/main/java/com/fernando/iop/user/dto/UserRecoveryConfirmRequestDTO.java
@@ -1,11 +1,24 @@
 package com.fernando.iop.user.dto;
 
+import jakarta.validation.constraints.*;
+
 import java.util.UUID;
 
 public record UserRecoveryConfirmRequestDTO(
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "O formato do e-mail é inválido")
+        @Size(min = 11, max = 64, message = "O e-mail deve ter entre 11 e 64 caracteres")
         String email,
+
+        @NotNull(message = "O ID do projeto é obrigatório")
         UUID projectId,
+
+        @NotBlank(message = "A nova senha é obrigatória")
+        @Size(min = 6, max = 64, message = "A senha deve ter entre 6 e 64 caracteres")
         String passWord,
+
+        @NotBlank(message = "O token de recuperação é obrigatório")
+        @Pattern(regexp = "^\\d{6}$", message = "O token de recuperação deve conter exatamente 6 dígitos numéricos")
         String recoveryToken
 ) {
 }
