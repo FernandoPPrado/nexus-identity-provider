e1bba8a31fcd4a32735e55850f0ee62fabac623e
Fernando Prado <fernandopradoifc@gmail.com>
2026-07-14 20:24:43 -0300

    feat(email): adiciona pool de concorrencia para rabbitMq

--- src/main/java/com/fernando/emailModule/listener/EmailRabbitListener.java
+++ src/main/java/com/fernando/emailModule/listener/EmailRabbitListener.java
@@ -2,8 +2,14 @@ package com.fernando.emailModule.listener;
 
 import com.fernando.emailModule.service.EmailService;
 import com.fernando.iop.user.dto.EmailEventDTO;
+import org.apache.tomcat.util.threads.VirtualThreadExecutor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class EmailRabbitListener {
@@ -14,7 +20,8 @@ public class EmailRabbitListener {
         this.emailService = emailService;
     }
 
-    @RabbitListener(queues = "fila-email")
+
+    @RabbitListener(queues = "fila-email", concurrency = "5")
     public void processarFila(EmailEventDTO evento) {
         emailService.emailSender(evento.email(), evento.token(), evento.tipoEventoEnum().name());
     }
