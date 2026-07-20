6bb889deb74a8562e886b17766a1d23af1357180
Fernando Prado <fernandopradoifc@gmail.com>
2026-07-19 22:32:02 -0300

    feat(exceptio): adiciona logs para o CustomExceptionHandler

--- src/main/java/com/fernando/iop/exceptions/customHandler/CustomExceptionHandler.java
+++ src/main/java/com/fernando/iop/exceptions/customHandler/CustomExceptionHandler.java
@@ -2,6 +2,8 @@ package com.fernando.iop.exceptions.customHandler;
 
 import com.fernando.iop.exceptions.dto.CustomExceptionDTO;
 import com.fernando.iop.exceptions.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,16 +14,20 @@ import java.time.Instant;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(CustomExceptionHandler.class);
 
     @ExceptionHandler({UserNotFoundException.class, InvalidCredentialsException.class})
     public ResponseEntity<CustomExceptionDTO> handleUserNotFound(RuntimeException ex) {
 
+        log.warn("Falha de autenticação: {}", ex.getMessage());
         CustomExceptionDTO customExceptionDTO = new CustomExceptionDTO(Instant.now(), HttpStatus.UNAUTHORIZED.value(), "E-mail ou senha incorretos");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(customExceptionDTO);
     }
 
     @ExceptionHandler({UserAlreadyExistsException.class})
     public ResponseEntity<CustomExceptionDTO> handleUserExists(UserAlreadyExistsException ex) {
+
+        log.warn("Falha criacao usuario: {}", ex.getMessage());
 
         CustomExceptionDTO customExceptionDTO = new CustomExceptionDTO(Instant.now(), HttpStatus.OK.value(), "Ok");
         return ResponseEntity.status(HttpStatus.OK).body(customExceptionDTO);
@@ -30,12 +36,16 @@ public class CustomExceptionHandler {
     @ExceptionHandler({TokenAlreadySentException.class})
     public ResponseEntity<CustomExceptionDTO> handleTokenExists(TokenAlreadySentException ex) {
 
+        log.warn("Falha Token: {}", ex.getMessage());
+
         CustomExceptionDTO customExceptionDTO = new CustomExceptionDTO(Instant.now(), HttpStatus.BAD_REQUEST.value(), "Verifique seu e-mail. Um link de confirmação já foi enviado recentemente.");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(customExceptionDTO);
     }
 
     @ExceptionHandler(InvalidTokenException.class)
     public ResponseEntity<CustomExceptionDTO> handleInvalidToken(InvalidTokenException ex) {
+
+        log.warn("Falha Token: {}", ex.getMessage());
 
         CustomExceptionDTO customExceptionDTO = new CustomExceptionDTO(
                 Instant.now(),
@@ -48,6 +58,8 @@ public class CustomExceptionHandler {
     @ExceptionHandler(ProjectNotFoundException.class)
     public ResponseEntity<CustomExceptionDTO> handleProjectNotFound(ProjectNotFoundException ex) {
 
+        log.warn("Falha projeto: {}", ex.getMessage());
+
         CustomExceptionDTO customExceptionDTO = new CustomExceptionDTO(
                 Instant.now(),
                 HttpStatus.NOT_FOUND.value(),
@@ -59,6 +71,8 @@ public class CustomExceptionHandler {
     @ExceptionHandler(UserAlreadyConfirmedException.class)
     public ResponseEntity<CustomExceptionDTO> handleUserAlreadyConfirmed(UserAlreadyConfirmedException ex) {
 
+        log.warn("Falha Confirmacao: {}", ex.getMessage());
+
         CustomExceptionDTO customExceptionDTO = new CustomExceptionDTO(
                 Instant.now(),
                 HttpStatus.CONFLICT.value(),
@@ -69,6 +83,7 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CustomExceptionDTO> handleGenericException(Exception ex) {
+        log.error("Erro interno, Motivo {}", ex.getMessage(), ex);
         CustomExceptionDTO customExceptionDTO = new CustomExceptionDTO(
                 Instant.now(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
